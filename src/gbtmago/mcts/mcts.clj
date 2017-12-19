0770c275a7c9409cc2cbb72e968a6f7d062d190b
Marian Gönninger <marian.goenninger@goenninger.net>
2017-12-19 10:55:59 +0100

    added zero mcts

--- src/gbtmago/mcts/mcts.clj
+++ src/gbtmago/mcts/mcts.clj
@@ -2,6 +2,86 @@
   (:require [clojure.tools.trace :as trace])
   (:use [rhizome.viz]))
 
+(defn puct [{:keys [c p n-sum n]}]
+  (* (* c p)
+     (/ (Math/sqrt n-sum)
+        (+ 1 n))))
+
+(defn get-prepared-probabilities [action-prob-dist size]
+  (doall (for [y (range size)
+               x (range size)]
+           (if (action-prob-dist [y x])
+             (action-prob-dist [y x])
+             0))))
+
+(defn zero-mcts [{:keys [tree root state deepend transition-fn actions-fn]}]
+  (if (:expanded (tree state))
+    (do (comment "SELECTION")
+        (let [children (-> (tree state)
+                           :children)
+              n-sum (reduce + (map (comp :n tree) children))
+              selected-child
+              (first (sort-by #(+ (:q (tree %))
+                                  (puct {:c 2
+                                         :p (:p (tree %))
+                                         :n (:n (tree %))
+                                         :n-sum n-sum}))
+                              (if (= (:player (tree state))
+                                     (:player (tree root)))
+                                >
+                                <)
+                              children))]
+          (merge tree
+                 (mcts {:tree tree
+                        :root root
+                        :state selected-child
+                        :deepend deepend
+                        :transition-fn transition-fn
+                        :actions-fn actions-fn}))))
+    (do (comment "EXPANSION AND EVALUATION")
+        (let [children (map (partial transition-fn state)
+                            (actions-fn state))
+              deepend-output (deepend "predict" {:state (:board state)})
+              action-probabilities (zipmap (doall (for [y (range (count ((:board state) 0)))
+                                                        x (range (count ((:board state) 0)))]
+                                                    [y x]))
+                                           (butlast deepend-output))
+              v (last deepend-output)
+              children-probabilities (reduce (fn [acc c]
+                                               (assoc acc c (action-probabilities (:action c))))
+                                             {}
+                                             children)
+              tree (reduce (fn [t c]
+                             (assoc t c {:n 0
+                                         :w 0
+                                         :expanded false
+                                         :q 0
+                                         :p (children-probabilities c)
+                                         :parent state}))
+                           tree
+                           children)
+              tree (assoc-in tree [state :children] children)
+              tree (loop [t tree
+                          n state
+                          new-t
+                          (-> t (update n (fn [o]
+                                            (let [new-n (inc (:n o))
+                                                  new-w (+ (:w o)
+                                                           (* v
+                                                              (if (= (:player (tree n))
+                                                                     (:player (tree root)))
+                                                                1
+                                                                -1)))
+                                                  new-q (/ new-w new-n)]
+                                              (-> o
+                                                  (assoc :n new-n)
+                                                  (assoc :w new-w)
+                                                  (assoc :q new-q))))))]
+                     (if (not= n root)
+                       (recur new-t (:parent n))
+                       new-t))]
+          tree))))
+
 (defn mcts [{:keys [tree root state actions-fn transition-fn terminal? terminal-r-fn expansion-policy selection-policy selection-policy-args simulation-fn simulation-policy value-update]}]
   (let [children (map (partial transition-fn state)
                       (actions-fn state))
